5a70417b9d82c93d70e36be6edc48aa2efb7c00f
NTT <truongnt@sotanext.com>
2023-04-05 18:04:41 +0700

    + add electron package

--- app.ts
+++ app.ts
@@ -1,4 +1,34 @@
+import {app,BrowserWindow} from 'electron';
+import path from 'path';
 import WalletGen from "./wallet-gen";
 
-var generator = new WalletGen();
-generator.autoGen();
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width:600,
+        height:480,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    });
+  });
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  });
+
+//var generator = new WalletGen();
+//generator.autoGen();
